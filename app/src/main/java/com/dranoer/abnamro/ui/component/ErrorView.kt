80a1bba28591f0858396ddabb6b1d2cac44f29a9
dranoer <dranoer@gmail.com>
2022-10-14 14:14:28 +0200

    add: Component view

---
+++ app/src/main/java/com/dranoer/abnamro/ui/component/ErrorView.kt
@@ -0,0 +1,26 @@
+package com.dranoer.abnamro.ui.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.dranoer.abnamro.R
+
+@Composable
+fun ErrorView(message: String, refresh: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(message)
+        Spacer(Modifier.height(16.dp))
+        Button(onClick = refresh) {
+            Text(text = stringResource(R.string.retry))
+        }
+    }
+}

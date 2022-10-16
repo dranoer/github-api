8299b7b5490eae062f0a639e90a9c256dad9a09a
dranoer <dranoer@gmail.com>
2022-10-16 23:43:09 +0200

    add: Test

---
+++ app/src/test/java/com/dranoer/abnamro/MainViewModelTest.kt
@@ -0,0 +1,78 @@
+package com.dranoer.abnamro
+
+import android.content.Context
+import com.dranoer.abnamro.domain.RepoRepository
+import com.dranoer.abnamro.ui.MainViewModel
+import com.dranoer.abnamro.ui.util.ViewState
+import org.hamcrest.MatcherAssert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.hamcrest.CoreMatchers.`is`
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.dranoer.abnamro.data.local.RepoDao
+import com.dranoer.abnamro.data.remote.WebService
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainViewModelTest {
+
+    @get:Rule
+    val rule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    @Mock
+    private lateinit var context: Context
+
+    @Mock
+    private lateinit var webService: WebService
+
+    @Mock
+    private lateinit var dao: RepoDao
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun `When response is okay THEN should return success`() {
+        testCoroutineRule.runBlockingTest {
+            `when`(webService.getRepoList(1, 10)).thenReturn(emptyList())
+            `when`(dao.getRepos()).thenReturn(emptyList())
+        }
+
+        val repository = RepoRepository(webService, dao)
+
+        testCoroutineRule.pauseDispatcher()
+        val viewModel = MainViewModel(repository)
+        MatcherAssert.assertThat(viewModel.stateFlow.value, `is`(ViewState.Loading))
+        testCoroutineRule.resumeDispatcher()
+        MatcherAssert.assertThat(viewModel.stateFlow.value, `is`(ViewState.Success(emptyList())))
+    }
+
+    @Test
+    fun `WHEN response is not okay THEN should return error`() {
+        val errorMsg = "Server error message"
+        `when`(context.getString(anyInt())).thenReturn(errorMsg)
+
+        testCoroutineRule.runBlockingTest {
+            `when`(webService.getRepoList(1, 10)).thenThrow(RuntimeException(""))
+            `when`(dao.getRepos()).thenReturn(emptyList())
+        }
+        val repository = RepoRepository(webService, dao)
+
+        testCoroutineRule.pauseDispatcher()
+        val viewModel = MainViewModel(repository)
+        MatcherAssert.assertThat(viewModel.stateFlow.value, `is`(ViewState.Loading))
+        testCoroutineRule.resumeDispatcher()
+        MatcherAssert.assertThat(viewModel.stateFlow.value, `is`(ViewState.Error(errorMsg)))
+    }
+}

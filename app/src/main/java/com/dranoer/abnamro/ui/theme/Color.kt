80cbd2b6ddeb6a1e277bbf8ac72b100338ca4f9b
dranoer <dranoer@gmail.com>
2022-10-16 15:43:45 +0200

    refactor: Theme

--- app/src/main/java/com/dranoer/abnamro/ui/theme/Color.kt
+++ app/src/main/java/com/dranoer/abnamro/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.dranoer.abnamro.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Grey = Color(0xFF5B6A7C)
+val DarkGrey = Color(0xFF7B8398)
+val LightBlue = Color(0xFFF0F5F7)
+val DarkBlue = Color(0xFF728397)
--- app/src/main/java/com/dranoer/abnamro/ui/theme/Theme.kt
+++ app/src/main/java/com/dranoer/abnamro/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = LightBlue,
+    primaryVariant = Grey,
+    secondary = Grey
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = DarkBlue,
+    primaryVariant = DarkBlue,
+    secondary = DarkGrey
 
     /* Other default colors to override
     background = Color.White,
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun AbnamroTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun GithubRepoTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
